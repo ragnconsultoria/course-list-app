12b1599a54633ddfe7168c0afe83ecfac5301de3
Develop Detail <86640194+ragnconsultoria@users.noreply.github.com>
2023-07-04 17:39:47 -0300

    #Atualização Layout Main, Criação de App Bar e Styles.

--- app/src/main/java/com/developdetail/courselistapp/MainActivity.java
+++ app/src/main/java/com/developdetail/courselistapp/MainActivity.java
@@ -1,14 +1,39 @@
 package com.developdetail.courselistapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    EditText etInputFirstName, etInputLastName, etInputCourseName, etInputPhoneNumber;
+    MaterialButton btnClear, btnSave, btnFinish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initializeFields();
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getResources().getString(R.string.app_name));
+    }
+
+    private void initializeFields() {
+        toolbar = findViewById(R.id.abl_main_activity);
+        etInputFirstName = findViewById(R.id.et_input_first_name_main_activity);
+        etInputLastName = findViewById(R.id.et_input_last_name_main_activity);
+        etInputCourseName = findViewById(R.id.et_input_course_name_main_activity);
+        etInputPhoneNumber = findViewById(R.id.et_input_phone_number_main_activity);
+        btnClear = findViewById(R.id.btn_clear_main_activity);
+        btnSave = findViewById(R.id.btn_save_main_activity);
+        btnFinish = findViewById(R.id.btn_finish_main_activity);
+
     }
 }
